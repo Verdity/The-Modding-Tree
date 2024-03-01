876c1dfea4605d54608698fcf8348f3f750a8181
Verdity <111776078+Verdity@users.noreply.github.com>
2024-03-01 19:43:00 +0100

    Update mod.js
    Changed the name and id!

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "The First Tree",
+	id: "MyFirstMod",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
