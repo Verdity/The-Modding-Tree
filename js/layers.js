2b8adf6fe349cc10f07d4657d88f9cdb6384df67
Verdity <111776078+Verdity@users.noreply.github.com>
2024-03-01 20:49:48 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -58,7 +58,7 @@ addLayer("p", {
 
 addLayer("d", {
     startData() { return {                  // startData is a function that returns default data for a layer. 
-        unlocked: true,                     // You can add more variables here to add them to your layer.
+        unlocked: false,                     // You can add more variables here to add them to your layer.
         points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
     }},
 
@@ -66,14 +66,14 @@ addLayer("d", {
     resource: "prestige points",            // The name of this layer's main prestige resource.
     row: 0,                                 // The row this layer is on (0 is the first row).
 
-    baseResource: "points",                 // The name of the resource your prestige gain is based on.
+    baseResource: "GigaReset points",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.points },  // A function to return the current amount of baseResource.
 
-    requires: new Decimal(10),              // The amount of the base needed to  gain 1 of the prestige currency.
+    requires: new Decimal(100),              // The amount of the base needed to  gain 1 of the prestige currency.
                                             // Also the amount required to unlock the layer.
 
     type: "normal",                         // Determines the formula used for calculating prestige currency.
-    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
+    exponent: 0,                          // "normal" prestige gain is (currency^exponent).
 
     gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
         return new Decimal(1)               // Factor in any bonuses multiplying gain here.
@@ -82,7 +82,7 @@ addLayer("d", {
         return new Decimal(1)
     },
 
-    layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
+    layerShown() { unlocked },          // Returns a bool for if this layer's node should be visible in the tree.
 
     upgrades: {
         // Look in the upgrades docs to see what goes here!
